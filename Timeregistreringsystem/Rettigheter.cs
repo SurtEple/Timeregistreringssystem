05ed7d89e61243cde513373c77cea019c9f45dcd
KristinaMG <kristina_gundersen@msn.com>
2014-04-07 14:36:14 +0200

    Konstanter

--- Timeregistreringsystem/Rettigheter.cs
+++ Timeregistreringsystem/Rettigheter.cs
@@ -7,9 +7,9 @@ namespace Timeregistreringssystem
 {
     public static class Rettigheter
     {
-        public const int Rettighet_vanlig_bruker = 0;
-        public const int Rettighet_team_leder = 1;
-        public const int Rettighet_prosjekt_ansvarlig = 2;
-        public const int Rettighet_administrator = 3;
+        public const int VANLIG_BRUKER = 0;
+        public const int TEAMLEDER = 1;
+        public const int PROSJEKT_ANSVARLIG = 2;
+        public const int ADMINISTRATOR = 3;
     }
 }
