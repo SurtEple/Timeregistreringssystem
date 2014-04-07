ebf4ae75cd2fc9aaa0e47d9ca8dc822bda6ae83e
KristinaMG <kristina_gundersen@msn.com>
2014-04-07 14:31:38 +0200

    Endret litt på syntaks i Rettigheter

--- Timeregistreringsystem/Rettigheter.cs
+++ Timeregistreringsystem/Rettigheter.cs
@@ -7,9 +7,9 @@ namespace Timeregistreringssystem
 {
     public static class Rettigheter
     {
-        public static int Rettighet_vanlig_bruker = 0;
-        public static int Rettighet_team_leder = 1;
-        public static int Rettighet_prosjekt_ansvarlig = 2;
-        public static int Rettighet_system_ansvarlig = 3;
+        public const int Rettighet_vanlig_bruker = 0;
+        public const int Rettighet_team_leder = 1;
+        public const int Rettighet_prosjekt_ansvarlig = 2;
+        public const int Rettighet_administrator = 3;
     }
 }
