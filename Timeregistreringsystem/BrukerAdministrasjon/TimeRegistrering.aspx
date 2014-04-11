e0620d8de7740b8e055291ef36deade5e0651021
SurtEpleTA <kallegne@gmail.com>
2014-04-11 11:06:06 +0200

    Timeregistrering

---
+++ Timeregistreringsystem/BrukerAdministrasjon/TimeRegistrering.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TimeRegistrering.aspx.cs" Inherits="Timeregistreringssystem.BrukerAdministrasjon.TimeRegistrering" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <asp:Button ID="ButtonStart" runat="server" OnClick="Button1_Click" Text="Start" Width="96px" />
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    <asp:Button ID="ButtonPause" runat="server" Height="26px" OnClick="Button2_Click" Text="Pause" Width="111px" />
+    <asp:Label ID="LabelPause" runat="server" Text="Label"></asp:Label>
+    <asp:Button ID="ButtonStop" runat="server" OnClick="Button3_Click" Text="Stop" Width="104px" />
+    <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" InsertCommand="INSERT INTO Timer(Start) VALUES (@Start)" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT Start, Pause, Slutt FROM Timer"></asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" InsertCommand="INSERT INTO Timer(Pause) VALUES (@Pause)" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT Pause FROM Timer"></asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSource3" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" InsertCommand="INSERT INTO Timer(Slutt) VALUES (@Slutt)" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT Slutt FROM Timer"></asp:SqlDataSource>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1">
+        <Columns>
+            <asp:BoundField DataField="Start" HeaderText="Start" SortExpression="Start" />
+            <asp:BoundField DataField="Pause" HeaderText="Pause" SortExpression="Pause" />
+            <asp:BoundField DataField="Slutt" HeaderText="Slutt" SortExpression="Slutt" />
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Timeregistreringsystem/BrukerAdministrasjon/TimeRegistrering.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Windows.Forms;
+/*
+ * @Author Thomas
+ * 2014 April
+ * */
+
+namespace Timeregistreringssystem.BrukerAdministrasjon
+{
+    public partial class TimeRegistrering : System.Web.UI.Page
+    {
+     //   private double timeElapsed = 0.0;
+
+        private DateTime dateStart = new DateTime();
+        private DateTime dateStop = new DateTime();
+        private DateTime dateTotal = new DateTime();
+        private DateTime datePause = new DateTime();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            dateStart = DateTime.Now;
+            Session["StartDato"] = dateStart;
+
+            SqlDataSource1.InsertParameters.Add("Start",dateStart.ToString("u"));
+            SqlDataSource1.Insert();
+            Label1.Text = dateStart.ToString();            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+             
+            datePause = DateTime.Now;
+            Session["PauseDato"] = datePause;
+
+            //dateStart = Convert.ToDateTime(Session["StartDato"].ToString());
+        //    dateStart = Convert.ToDateTime(Session["StartDato"]);
+       //     dateTotal = Convert.ToDateTime(datePause.Subtract(dateStart).ToString());
+
+      //      Label3.Text = "Differanse: " + (datePause.Subtract(dateStart) + "totaltid:" + dateTotal.ToString());
+        //    Label2.Text = "StartTid:" + dateStart.TimeOfDay.ToString();
+
+            SqlDataSource2.InsertParameters.Add("Pause", datePause.ToString("u"));
+            SqlDataSource2.Insert();
+           
+            GridView1.DataBind();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            dateStop = DateTime.Now;
+      //      Session["SluttDato"] = dateStop;
+
+      //      dateStart = Convert.ToDateTime(Session["StartDato"]);
+     //       datePause = Convert.ToDateTime(Session["PauseDato"]);
+    //        dateStop = Convert.ToDateTime(Session["SluttDato"]);
+
+       //    DateTime dateTotal2 = Convert.ToDateTime(dateStop.Subtract(dateStart).ToString());
+        //    dateTotal2.Subtract(datePause);
+        
+            SqlDataSource3.InsertParameters.Add("Slutt", dateStop.ToString("u"));
+            SqlDataSource3.Insert();
+            GridView1.DataBind();
+          //  dateStop = Convert.ToDateTime(dateStop.Subtract(dateStart));
+          //  dateStop.Subtract(dateTotal);
+            
+            Label3.Text = "ArbeidsTid:"+ dateTotal.ToString(); 
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
