3e041cc9ff931b718e17118ca4dd452fa2ed5ec9
BRPedersen <brpedersen47@gmail.com>
2014-05-12 13:35:56 +0200

    Startside for brukeren
    Kunne sikkert gjort det på en bedre måte, både med tanke på koding og
    utseende, men det funker vertfall :P
    ... og nå har jeg jinxet det i tillegg, yay.

---
+++ Timeregistreringsystem/Startside/BrukerForside.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BrukerForside.aspx.cs" Inherits="Timeregistreringssystem.BrukerForside" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Velkommen, <%= (string)Session["Brukernavn"] %> </h1>
+    <h2>Velg din oppgave.</h2>
+
+    <table>
+    
+         <!-- Hvis du er med i flere team, velg ett -->
+        <tr>
+            <td style="width: 352px" class="modal-sm">
+                <asp:Label ID="lblVelgTeam" runat="server" Text="Velg den gruppen du vil jobbe med"></asp:Label>
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlVelgTeam" runat="server" DataSourceID="SqlDataSourceDDLVelgTeam" DataTextField="Beskrivelse" DataValueField="ID" AppendDataBoundItems="true">
+                    <asp:ListItem Text="-- Velg et Team --" Value=""></asp:ListItem>
+                </asp:DropDownList>
+
+                <asp:SqlDataSource ID="SqlDataSourceDDLVelgTeam" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM Team WHERE Team.ID in (SELECT Team_ID FROM KoblingBrukerTeam WHERE Bruker_ID = @ID)"></asp:SqlDataSource>
+
+            </td>
+            <td>
+                <asp:Button ID="btnTeamBekreft" runat="server" Text="Bekreft" OnClick="btnTeamBekreft_Click" />
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <asp:Label ID="lblIkkeValgtTeam" runat="server" Text="Velg et Team fra listen" Visible="False" ForeColor="Red"></asp:Label>
+            </td>
+        </tr>
+
+         <!-- Velg Prosjekt -->
+         <tr>
+             <td style="height: 100px; width: 352px;">
+                 <asp:Label ID="lblVelgProsjekt" runat="server" Text="Velg Prosjektet du vil jobbe med i dag"></asp:Label>
+             </td>
+             <td style="height: 100px">
+                <asp:DropDownList ID="ddlVelgProsjekt" runat="server" DataSourceID="SqlDataSourceDDLVelgProsjekt" DataTextField="Navn" DataValueField="ID" AppendDataBoundItems="true">
+                    <asp:ListItem Text="-- Velg Prosjekt --" Value="" ></asp:ListItem>
+                </asp:DropDownList>
+                 <asp:SqlDataSource ID="SqlDataSourceDDLVelgProsjekt" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM Prosjekt WHERE Prosjekt.ID in (SELECT Prosjekt_ID FROM KoblingTeamProsjekt WHERE Team_ID = @TeamID)"></asp:SqlDataSource>
+             </td>
+             <td style="height: 100px">
+                 <asp:Button ID="btnProsjektBekreft" runat="server" Text="Bekreft" OnClick="btnProsjektBekreft_Click"  />
+             </td>
+        </tr>
+
+        <!-- Valgte ikke et Prosjekt -->  
+        <tr>
+            <td>
+                <asp:Label ID="lblIkkeValgtProsjekt" runat="server" Text="Velg et Prosjekt fra listen" Visible="False" ForeColor="Red"></asp:Label>
+            </td>
+        </tr>
+
+
+        <!-- Velg Hovedoppgave -->
+        <tr>
+            <td style="width: 352px" class="modal-sm">
+                <asp:Label ID="lblVelgHovedoppgave" runat="server" Text="Velg oppgaven du vil jobbe med i dag"></asp:Label>
+            </td>
+            <td>
+                <asp:DropDownList ID="ddlVelgHovedoppgave" runat="server" DataSourceID="SqlDataSourceVelgHovedoppgave" DataTextField="Tittel" DataValueField="ID" AppendDataBoundItems="true">
+                    <asp:ListItem Text="-- Velg Oppgave -- " Value ="" ></asp:ListItem>
+                </asp:DropDownList>
+                <asp:SqlDataSource ID="SqlDataSourceVelgHovedoppgave" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT ID, Foreldreoppgave_ID, Prosjekt_ID, Tittel, Beskrivelse, Ferdig, Brukt_tid AS &quot;Brukt tid&quot;, DATE_FORMAT(Dato_begynt, '%Y-%m-%d') AS &quot;Startdato&quot;, DATE_FORMAT(Dato_ferdig, '%Y-%m-%d') AS &quot;Sluttdato&quot; FROM `Oppgave` WHERE `Foreldreoppgave_ID` LIKE 0 AND Prosjekt_ID = @Prosjekt_ID"></asp:SqlDataSource>
+            </td>
+            <td>
+                <asp:Button ID="btnHovedoppgaveBekreft" runat="server" Text="Bekreft" OnClick="btnHovedoppgaveBekreft_Click" />
+            </td>
+        </tr> 
+
+    </table>
+
+    <!-- Valgt Prosjekt har ingen Oppgaver tilordnet -->
+    <asp:Label ID="lblIngenOppgaver" runat="server" Text="Velg en oppgave fra listen. (Hvis listen er tom så har ikke dette prosjektet noen oppgaver)" Visible="False" ForeColor="Red"></asp:Label>
+          
+    <br /> 
+
+    <!-- Velg Oppgave -->
+ 
+            <asp:GridView ID="GridViewVelgOppgave" runat="server" AutoGenerateColumns="False"
+        CssClass="table" AllowPaging="True" AllowSorting="True" Width="80%" OnSelectedIndexChanged="GridViewVelgHovedppgave_SelectedIndexChanged" DataSourceID="SqlDataSourceVelgOppgave">
+                <Columns>
+                    <asp:BoundField DataField="Tittel" HeaderText="Tittel" SortExpression="Tittel" />
+                    <asp:BoundField DataField="Beskrivelse" HeaderText="Beskrivelse" SortExpression="Beskrivelse" />
+                    <asp:CheckBoxField DataField="Ferdig" HeaderText="Ferdig" SortExpression="Ferdig" />
+                    <asp:BoundField DataField="Brukt tid" HeaderText="Brukt tid" SortExpression="Brukt tid" />
+                    <asp:BoundField DataField="Startdato" HeaderText="Startdato" SortExpression="Startdato" />
+                    <asp:BoundField DataField="Sluttdato" HeaderText="Sluttdato" SortExpression="Sluttdato" />
+                </Columns>
+        </asp:GridView>
+
+        <asp:SqlDataSource ID="SqlDataSourceVelgOppgave" runat="server" ConnectionString="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString %>" ProviderName="<%$ ConnectionStrings:HLVDKN_DB1ConnectionString.ProviderName %>" SelectCommand="SELECT Tittel, Beskrivelse, Ferdig, Brukt_tid AS  &quot;Brukt tid&quot;, DATE_FORMAT( Dato_begynt,  '%Y-%m-%d' ) AS  &quot;Startdato&quot;, DATE_FORMAT( Dato_ferdig,  '%Y-%m-%d' ) AS  &quot;Sluttdato&quot; FROM  `Oppgave` WHERE `Foreldreoppgave_ID` = @Foreldreoppgave_ID"></asp:SqlDataSource>
+
+    <br />
+
+    <asp:Button ID="btnTilbakeTilTeam" runat="server" Text="Tilbake" OnClick="btnTilbakeTilTeam_Click" />
+
+    </asp:Content>
---
+++ Timeregistreringsystem/Startside/BrukerForside.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Timeregistreringssystem
+{
+    public partial class BrukerForside : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int rettighet = Convert.ToInt32(Session["Admin"]);
+
+            if ((rettighet == Rettigheter.VANLIG_BRUKER || rettighet == Rettigheter.TEAMLEDER) && Global.CheckIP())
+            {
+                // Henter ut brukerens ID ved innlogging
+                int BrukerId = (int)Session["BrukerID"]; // Tar vare på den inlogged brukerens ID
+                SqlDataSourceDDLVelgTeam.SelectParameters.Add("ID", BrukerId.ToString()); // Setter inn verdien til Brukerens id i Select query.
+
+                // Setter "Velg Prosjekt" til å ikke vises før "Team" er valgt
+                lblVelgProsjekt.Visible = false;
+                ddlVelgProsjekt.Visible = false;
+                btnProsjektBekreft.Visible = false;
+                btnTilbakeTilTeam.Visible = false;
+
+                // Setter "Velg Hovedoppgave" til å ikke vises før "Prosjekt" er valgt
+                lblVelgHovedoppgave.Visible = false;
+                ddlVelgHovedoppgave.Visible = false;
+                btnHovedoppgaveBekreft.Visible = false;
+
+                // Setter "Velg Oppgave" til å ikke vises før "Hovedoppgave" er valgt
+                GridViewVelgOppgave.Visible = false;
+            }
+            else
+                Response.Redirect("~/Default");
+            
+        }
+
+        // Knapp som tar deg videre til "Velg Prosjekt"
+        protected void btnTeamBekreft_Click(object sender, EventArgs e)
+        {
+            if (ddlVelgTeam.SelectedValue == "")
+            {
+                // Har ikke valgt Team
+                lblIkkeValgtTeam.Visible = true;
+            }
+            else
+            {
+                lblIkkeValgtTeam.Visible = false;
+
+                //Setter alle "Team" felt og knapper til å være "Deaktivert"
+                lblVelgTeam.Enabled = false;
+                ddlVelgTeam.Enabled = false;
+                btnTeamBekreft.Enabled = false;
+
+                // Viser alle "Prosjekt" knapper og felt
+                lblVelgProsjekt.Visible = true;
+                ddlVelgProsjekt.Visible = true;
+                btnProsjektBekreft.Visible = true;
+                btnTilbakeTilTeam.Visible = true;
+
+            
+                int TeamId = Convert.ToInt32(ddlVelgTeam.SelectedValue); // Tar vare på valgt Team-ID
+                SqlDataSourceDDLVelgProsjekt.SelectParameters.Add("TeamID", TeamId.ToString()); // Setter inn verdien til brukerens valgte team i Select query
+       
+            }
+
+             }
+
+        //Knapp som tar deg videre til "Velg Hovedoppgave"
+        protected void btnProsjektBekreft_Click(object sender, EventArgs e)
+        {
+            if (ddlVelgProsjekt.SelectedValue == "")
+            {
+                // Ingen prosjekt er valgt
+                lblIkkeValgtProsjekt.Visible = true;
+
+                // La brukeren velge et annet Prosjekt
+                lblVelgProsjekt.Visible = true;
+                ddlVelgProsjekt.Visible = true;
+                btnProsjektBekreft.Visible = true;
+                btnTilbakeTilTeam.Visible = true;
+            }
+            else
+            {
+                lblIkkeValgtProsjekt.Visible = false;
+
+                // Tar vare på alle "Prosjekt" knapper og felt men setter dem som "Deaktivert"
+                lblVelgProsjekt.Enabled = false;
+                ddlVelgProsjekt.Enabled = false;
+                btnProsjektBekreft.Enabled = false;
+
+                lblVelgProsjekt.Visible = true;
+                ddlVelgProsjekt.Visible = true;
+                btnProsjektBekreft.Visible = true;
+                btnTilbakeTilTeam.Visible = true;
+
+            
+                lblVelgHovedoppgave.Visible = true;
+                ddlVelgHovedoppgave.Visible = true;
+                btnHovedoppgaveBekreft.Visible = true;
+
+                int Prosjekt_ID = Convert.ToInt32(ddlVelgProsjekt.SelectedValue);
+                SqlDataSourceVelgHovedoppgave.SelectParameters.Add("Prosjekt_ID", Prosjekt_ID.ToString());
+            }
+            
+            
+
+        }
+
+        // Knapp som tar deg videre til "Velg Deloppgave" i GridView
+        protected void btnHovedoppgaveBekreft_Click(object sender, EventArgs e)
+        {
+            if (ddlVelgHovedoppgave.SelectedValue == "")
+            {
+                lblIngenOppgaver.Visible = true;
+                
+                lblVelgProsjekt.Enabled = false;
+                ddlVelgProsjekt.Enabled = false;
+                btnProsjektBekreft.Enabled = false;
+
+
+                lblVelgProsjekt.Visible = true;
+                ddlVelgProsjekt.Visible = true;
+                btnProsjektBekreft.Visible = true;
+                btnTilbakeTilTeam.Visible = true;
+
+                lblVelgHovedoppgave.Enabled = true;
+                ddlVelgHovedoppgave.Enabled = true;
+                btnHovedoppgaveBekreft.Enabled = true;
+
+                lblVelgHovedoppgave.Visible = true;
+                ddlVelgHovedoppgave.Visible = true;
+                btnHovedoppgaveBekreft.Visible = true;
+            }
+            else
+            {
+                lblIngenOppgaver.Visible = false;
+
+                // Tar vare på alle knapper og felt men setter dem som "Deaktivert"
+                lblVelgProsjekt.Enabled = false;
+                ddlVelgProsjekt.Enabled = false;
+                btnProsjektBekreft.Enabled = false;
+
+
+                lblVelgProsjekt.Visible = true;
+                ddlVelgProsjekt.Visible = true;
+                btnProsjektBekreft.Visible = true;
+                btnTilbakeTilTeam.Visible = true;
+
+                lblVelgHovedoppgave.Enabled = false;
+                ddlVelgHovedoppgave.Enabled = false;
+                btnHovedoppgaveBekreft.Enabled = false;
+
+                lblVelgHovedoppgave.Visible = true;
+                ddlVelgHovedoppgave.Visible = true;
+                btnHovedoppgaveBekreft.Visible = true;
+
+
+                int Foreldreoppgave_ID = Convert.ToInt32(ddlVelgHovedoppgave.SelectedValue);
+                SqlDataSourceVelgOppgave.SelectParameters.Add("Foreldreoppgave_ID", Foreldreoppgave_ID.ToString());
+
+                GridViewVelgOppgave.Visible = true;
+            }
+            
+        }
+
+        // Metode for å velge "Deloppgave" i GridView
+        protected void GridViewVelgHovedppgave_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Knapp som lar deg gå tilbake for å velge et annet Team.
+        protected void btnTilbakeTilTeam_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("BrukerForside.aspx");
+        }
+    } 
+    
+}
